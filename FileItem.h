3769f4e400210004ccdd6d2d3cc1c46e7d016227
nonamelive <nonamelive@gmail.com>
2011-01-29 15:52:38 +0800

    Added convert status.

--- FileItem.h
+++ FileItem.h
@@ -8,12 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+	kFileItemStatusWaiting = 0,
+	kFileItemStatusConverting,
+	kFileItemStatusSucceeded,
+	kFileItemStatusFailed,
+} FileItemStatus;
 
 @interface FileItem : NSObject {
 
 	NSString *name;
 	NSString *path;
 	NSImage *icon;
+	NSString *convertedFilename;
+	
+	FileItemStatus status;
+	NSImage *statusIcon;
     
     BOOL directory;
 	BOOL invisible;
@@ -22,8 +32,14 @@
 @property (nonatomic, copy) NSString *name;
 @property (nonatomic, copy) NSString *path;
 @property (nonatomic, retain) NSImage *icon;
+@property (nonatomic, retain) NSString *convertedFilename;
+
+@property (nonatomic, assign) FileItemStatus status;
+@property (nonatomic, retain) NSImage *statusIcon;
 
 @property (nonatomic, readonly, getter=isDirectory) BOOL directory;
 @property (nonatomic, readonly, getter=isInvisible) BOOL invisible;
+
+- (BOOL)isEqualToFileItem:(FileItem *)anItem;
 
 @end
--- FileItem.m
+++ FileItem.m
@@ -14,8 +14,52 @@
 @synthesize name;
 @synthesize path;
 @synthesize icon;
+@synthesize convertedFilename;
+@synthesize status;
+@synthesize statusIcon;
 @synthesize directory;
 @synthesize invisible;
+
+- (void)setStatus:(FileItemStatus)aStatus {
+	
+	status = aStatus;
+	
+	switch (status) {
+		case kFileItemStatusWaiting:
+			self.statusIcon = [NSImage imageNamed:@"waiting.png"];
+			break;
+		case kFileItemStatusConverting:
+			self.statusIcon = [NSImage imageNamed:@"converting.png"];
+			break;
+		case kFileItemStatusSucceeded:
+			self.statusIcon = [NSImage imageNamed:@"succeeded.png"];
+			break;
+		case kFileItemStatusFailed:
+			self.statusIcon = [NSImage imageNamed:@"failed.png"];
+			break;
+		default:
+			break;
+	}
+}
+
+- (void)setPath:(NSString *)aPath {
+	
+	if (path != aPath) {
+        [path release];
+        path = [aPath retain];
+		
+		self.name = [path lastPathComponent];
+		self.icon = [[NSWorkspace sharedWorkspace] iconForFile:path];
+		
+		// Generate the new filename. e.g. nonamelive.txt -> nonamelive.converted.txt
+		NSString *newFilename = [self.name stringByDeletingPathExtension];
+		newFilename = [newFilename stringByAppendingPathExtension:@"converted"];
+		newFilename = [newFilename stringByAppendingPathExtension:[self.name pathExtension]];
+		self.convertedFilename = newFilename;
+		
+		self.status = kFileItemStatusWaiting;
+    }
+}
 
 - (BOOL)isDirectory {
     
@@ -27,18 +71,44 @@
 
 - (BOOL)isInvisible {
 	
+	if ([[[NSWorkspace sharedWorkspace] mountedLocalVolumePaths] containsObject:path]) {
+		return YES;
+	}
+	
 	CFURLRef inURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, (CFStringRef)path, kCFURLPOSIXPathStyle, ![self isDirectory]);
 	LSItemInfoRecord itemInfo;
 	LSCopyItemInfoForURL(inURL, kLSRequestAllFlags, &itemInfo);
 	
 	BOOL isInvisible = itemInfo.flags & kLSItemInfoIsInvisible;
+	
+	CFRelease(inURL);
+	
 	return (isInvisible != 0);
+}
+
+- (BOOL)isEqual:(id)other {
+    if (other == self)
+        return YES;
+    if (!other || ![other isKindOfClass:[self class]])
+        return NO;
+    return [self isEqualToFileItem:other];
+}
+
+- (BOOL)isEqualToFileItem:(FileItem *)anItem {
+    if (self == anItem)
+        return YES;
+    if (![(id)[self path] isEqual:[anItem path]])
+        return NO;
+
+    return YES;
 }
 
 - (void)dealloc {
 	[name release];
-	[path  release];
+	[path release];
 	[icon release];
+	[convertedFilename release];
+	[statusIcon release];
 	[super dealloc];
 }
 
